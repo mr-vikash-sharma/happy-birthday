5fee6a5f9ee7164030cd452fe04a3c7c67ffd3f8
Shivam Abraham <codingnerd84@gmail.com>
2021-12-23 03:00:46 +0530

    final touch added the menu functionaly and fully responsive for mobile

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const menu = document.querySelector(".menu");
 
+const navigationbtns = document.querySelectorAll(".navigationbtn");
 const list = document.querySelector(".list");
 
 menu.addEventListener('click',function(){
     list.classList.toggle("active-list");
     menu.classList.toggle("active-menu");
+});
+
+navigationbtns.forEach(function(btn){
+btn.addEventListener('click',function(){
+    list.classList.remove("active-list");
+    menu.classList.remove("active-menu");
+})
 })
